c36254f79d4648c544c58c298d253f3c76fc0760
jasutrb <43812984+jasutrb@users.noreply.github.com>
2020-05-02 15:04:59 +0900

    test2

---
+++ Recipe_Finder_1.0.1/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Recipe_Finder_1.0.1
+//
+//  Created by 釣部正義 on 2020/05/01.
+//  Copyright © 2020 jasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var keyword1 : String? = ""
+
+    @IBOutlet weak var ingredient: UILabel!
+    @IBOutlet weak var ingredient1: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBAction func searchRecipi(_ sender: UIButton) {
+        //labelTest.text = "検索！"
+        //let url = NSURL(string: "http://google.com")
+        //if UIApplication.shared.canOpenURL(url! as URL){
+        //    UIApplication.shared.openURL(url! as URL)
+        //}
+        UIApplication.shared.open(URL(string: "https://google.co.jp/search?q=banana")!, options: [:], completionHandler: nil)
+        sender.setTitle("検索！", for: .normal)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        ingredient.text = "材料は何を使う？"
+        
+        ingredient1.placeholder = "材料1"
+        ingredient1.returnKeyType = UIReturnKeyType.google //検索ボタン
+        ingredient1.clearButtonMode = UITextField.ViewMode.always //つねに表示
+        
+        searchButton.setTitle("準備OK!検索！", for: .normal)
+        
+
+    }
+
+    //テキストフィールドでリターンが押されたときに通知され起動するメソッド
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //textField.resignFirstResponder()　←↓どっちでもいい
+        self.view.endEditing(true)
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //画面外タッチで入力終了
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        keyword1 = ingredient1.text
+        self.view.endEditing(true)
+    }
+}
+
