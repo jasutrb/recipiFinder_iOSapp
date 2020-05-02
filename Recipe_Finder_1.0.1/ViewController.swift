7f7bfc64662985ee46f9b959136334eb109402d5
jasutrb <43812984+jasutrb@users.noreply.github.com>
2020-05-03 01:16:10 +0900

    3search

--- Recipe_Finder_1.0.1/ViewController.swift
+++ Recipe_Finder_1.0.1/ViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     var keyword1 : String! = ""
-
+    var keyword2 : String! = ""
+    var keyword3 : String! = ""
+    
     @IBOutlet weak var ingredient: UILabel!
     @IBOutlet weak var ingredient1: UITextField!
+    @IBOutlet weak var ingredient2: UITextField!
+    @IBOutlet weak var ingredient3: UITextField!
     @IBOutlet weak var searchButton: UIButton!
+    
+    //検索実行
     @IBAction func searchRecipi(_ sender: UIButton) {
-        let url : String! = "https://google.co.jp/search?q="
-        UIApplication.shared.open(URL(string : url + keyword1)!, options: [:], completionHandler: nil)
+        keyword1 = ingredient1.text
+        keyword2 = ingredient2.text
+        keyword3 = ingredient3.text
+        let urlBase : String! = "https://google.co.jp/search?q="
+        var url : String!
+        url = urlBase + keyword1 + "+" + keyword2 + "+" + keyword3
+        //現状英語検索しかできない
+        UIApplication.shared.open(URL(string : url)!, options: [:], completionHandler: nil)
         sender.setTitle("検索！", for: .normal)
     }
     
@@ -29,7 +41,15 @@ class ViewController: UIViewController {
         ingredient1.returnKeyType = UIReturnKeyType.google //検索ボタン
         ingredient1.clearButtonMode = UITextField.ViewMode.always //つねに表示
         
-        searchButton.setTitle("準備OK!検索！", for: .normal)
+        ingredient2.placeholder = "材料2"
+        ingredient2.returnKeyType = UIReturnKeyType.google //検索ボタン
+        ingredient2.clearButtonMode = UITextField.ViewMode.always //つねに表示
+        
+        ingredient3.placeholder = "材料3"
+        ingredient3.returnKeyType = UIReturnKeyType.google //検索ボタン
+        ingredient3.clearButtonMode = UITextField.ViewMode.always //つねに表示
+        
+        searchButton.setTitle("準備OK! 検索！", for: .normal)
         
 
     }
@@ -45,9 +65,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //画面外タッチで入力終了
+    
+    //画面外タッチでテキスト入力終了
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        keyword1 = ingredient1.text
         self.view.endEditing(true)
     }
 }
