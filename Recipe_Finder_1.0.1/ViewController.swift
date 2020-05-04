e3c62cf925aba26fa688e3d1edc085f5651b59ee
jasutrb <43812984+jasutrb@users.noreply.github.com>
2020-05-05 01:18:56 +0900

    JP search

--- Recipe_Finder_1.0.1/ViewController.swift
+++ Recipe_Finder_1.0.1/ViewController.swift
@@ -17,41 +17,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var ingredient1: UITextField!
     @IBOutlet weak var ingredient2: UITextField!
     @IBOutlet weak var ingredient3: UITextField!
+    //検索機能
     @IBOutlet weak var searchButton: UIButton!
-    
-    //検索実行
     @IBAction func searchRecipi(_ sender: UIButton) {
         keyword1 = ingredient1.text
         keyword2 = ingredient2.text
         keyword3 = ingredient3.text
+        //日本語に対応するためにパーセントエンコーディングにかける
+        let perKey1 : String! = percentEncording(keyword1)
+        let perKey2 : String! = percentEncording(keyword2)
+        let perKey3 : String! = percentEncording(keyword3)
+        
         let urlBase : String! = "https://google.co.jp/search?q="
         var url : String!
-        url = urlBase + keyword1 + "+" + keyword2 + "+" + keyword3
-        //現状英語検索しかできない
+        url = urlBase + perKey1 + "+" + perKey2 + "+" + perKey3
         UIApplication.shared.open(URL(string : url)!, options: [:], completionHandler: nil)
         sender.setTitle("検索！", for: .normal)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         ingredient.text = "材料は何を使う？"
-        
-        ingredient1.placeholder = "材料1"
-        ingredient1.returnKeyType = UIReturnKeyType.google //検索ボタン
-        ingredient1.clearButtonMode = UITextField.ViewMode.always //つねに表示
-        
-        ingredient2.placeholder = "材料2"
-        ingredient2.returnKeyType = UIReturnKeyType.google //検索ボタン
-        ingredient2.clearButtonMode = UITextField.ViewMode.always //つねに表示
-        
-        ingredient3.placeholder = "材料3"
-        ingredient3.returnKeyType = UIReturnKeyType.google //検索ボタン
-        ingredient3.clearButtonMode = UITextField.ViewMode.always //つねに表示
-        
+        keywordFormat( ingredient1, str: "1")
+        keywordFormat( ingredient2, str: "2")
+        keywordFormat( ingredient3, str: "3")
         searchButton.setTitle("準備OK! 検索！", for: .normal)
-        
-
+    }
+    
+    //検索ワード入力フォーマットのメソッド
+    func keywordFormat(_ textField: UITextField, str: String!){
+        textField.placeholder = "材料" + str
+        textField.returnKeyType = UIReturnKeyType.google //検索ボタン
+        textField.clearButtonMode = UITextField.ViewMode.always //つねに表示
+        //return true
+    }
+    
+    //パーセントエンコーディングの関数
+    func percentEncording(_ str: String!) -> String!{
+        return str.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
     }
 
     //テキストフィールドでリターンが押されたときに通知され起動するメソッド
